ae4889504083df4f6bec653e41ab2e5e71cd98db
Use your GitHub username <jer14002@byui.edu>
2025-05-06 22:13:29 -0400

    Week3team

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,5 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-  // If cart is not an array, make it a new array
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-  cart.push(product);
-  localStorage.setItem("so-cart", JSON.stringify(cart));
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam('product');
+productDetails(productId);
